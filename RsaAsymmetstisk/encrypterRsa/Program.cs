290df6da79cba574635414579e542e2669f92ffe
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-08-11 13:04:47 +0200

    Add files via upload

--- RsaAsymmetstisk/encrypterRsa/Program.cs
+++ RsaAsymmetstisk/encrypterRsa/Program.cs
@@ -19,34 +19,34 @@ internal class Program
 
         HexToByteArrayConverter Conv = new HexToByteArrayConverter();
 
-        // Modtager hexadecimal, find virkne convertering løsning
+        //modtager indput fra bruger og sender det til en klasse med en metode der convertere det fra en string med hex om til byte[].
         Console.WriteLine();
-        Console.WriteLine(" Indtast/paste Exponent :");
+        Console.Write(" Indtast/paste Exponent :");
         string exponentString = Console.ReadLine();
         byte[] exponent = Conv.StringToByteArray(exponentString);
 
         Console.WriteLine();
-        Console.WriteLine(" Indtast/paste Modulus :");
+        Console.Write(" Indtast/paste Modulus :");
         string modulusString = Console.ReadLine();
         byte[] modulus = Conv.StringToByteArray(modulusString);
 
+        // sender modulus og exponet til encrypter så den kan lave nøgle og erstatte modulus og exponet parameterne, med dem fra bruger indput
+        var rsaen = new Encrypter(modulus, exponent);
+
+        // modtager input fra bruger og laver det om til en bytes og sender det til EncryptData som så returnere den kryptered besked.
         Console.WriteLine();
         Console.Write(" Indtast besked : ");
         string? Message = Console.ReadLine();
-
-
-
-        var rsaen = new Encrypter(modulus, exponent);
-
         var encryptedMessage = rsaen.EncryptData(Encoding.ASCII.GetBytes(Message));
         rsaen.DeleteKey();
 
-        var lentghten = BitConverter.ToString(value: encryptedMessage).Replace("-", ""); ;
-        Console.WriteLine($"Encrypted Message Length: {lentghten.Length} bytes");
+
         
 
-        // output area for 
-        //Console.Clear();
+        // udskriver antal bytes og orginal besked og den kryptered besked, ved godt det ikke er sikkert, gør det for demostrations grundlag.
+        Console.Clear();
+        var lentghten = BitConverter.ToString(value: encryptedMessage).Replace("-", ""); ;
+        Console.WriteLine($" Encrypted Message Length: {lentghten.Length} bytes");
         Console.WriteLine();
         Console.WriteLine(" Original Text = " + Message);
         Console.WriteLine();
